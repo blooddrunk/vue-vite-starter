baa0ba91e1e5e67528c60577d435db7920487103
Xie Yuhao <jelinenaro@gmail.com>
2021-04-20 16:41:41 +0800

    chore: update dependencies

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,7 +3,7 @@
     <section class="tw-flex tw-flex-col tw-items-center tw-pt-20">
       <router-link to="/">
         <h2
-          class="tw-flex tw-items-center tw-py-5 tw-text-3xl tw-text-indigo-500 tw-font-bold"
+          class="tw-flex tw-items-center tw-py-5 tw-text-3xl tw-text-primary tw-font-bold"
         >
           <IconHome></IconHome>
           <span class="tw-ml-1">Vue 3</span>
