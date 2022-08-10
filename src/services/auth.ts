9d32a3fefbd96db15fd235d4f91bbbeeccdfa04a
Xie Yuhao <jelinenaro@gmail.com>
2022-08-10 17:49:12 +0800

    stage

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,10 @@
-import type { UserInfo, LoginInfo } from '@typings';
+import { random } from 'lodash-es';
+import type {
+  UserInfo,
+  LoginInfo,
+  MobileUserInfo,
+  MobileLoginInfo,
+} from '@typings';
 import { promiseTimeout } from '@/utils/misc';
 
 export const useLogin = () => {
@@ -24,6 +30,50 @@ export const useLogin = () => {
           payload.userName === 'admin'
             ? ['dashboard', 'admin', 'admin-account', 'admin-role']
             : ['dashboard', 'admin', 'admin-role'],
+      };
+      return data.value;
+    } catch (e: any) {
+      error.value = e.message;
+      console.error(e);
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+  return {
+    data,
+    isLoading,
+    error,
+    loginRequest,
+  };
+};
+
+export const fetchAuthCode = async (mobile: string) => {
+  await promiseTimeout(1000);
+  return random(1000);
+};
+
+export const useAuthCode = () => {};
+
+export const useMobileLogin = () => {
+  const data = ref<MobileUserInfo>();
+  const isLoading = ref(false);
+  const error = ref<string | null>('');
+
+  const loginRequest = async (
+    payload: MobileLoginInfo
+  ): Promise<MobileUserInfo | undefined> => {
+    isLoading.value = true;
+
+    try {
+      await promiseTimeout(1500);
+
+      if (payload.authCode !== '123456') {
+        throw new Error(`wrong auth info`);
+      }
+
+      data.value = {
+        mobile: payload.mobile,
       };
       return data.value;
     } catch (e: any) {
